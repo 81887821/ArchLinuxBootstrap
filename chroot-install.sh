843fedfb443ee9230136eda117b9a57ef6479662
YeonGyu Jeong <81887821@hanmail.net>
2018-01-30 23:24:51 +0900

    Add install scripts.

---
+++ chroot-install.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# This script should be run in chroot environment.
+
+readonly LINUX_LTS_PACKAGES='linux-lts linux-lts-headers'
+readonly BTRFS_PACKAGES='btrfs-progs'
+readonly NTFS_PACKAGES='ntfs-3g'
+readonly GUI_PACKAGES='xorg-server xorg-xrandr xfce4 xfce4-goodies lightdm lightdm-gtk-greeter'
+readonly NETWORK_PACKAGES='networkmanager network-manager-applet networkmanager-pptp wpa_supplicant'
+readonly SOUND_PACKAGES='pavucontrol'
+readonly INPUT_METHOD_PACKAGES='ibus ibus-hangul ibus-anthy'
+readonly FONT_PACKAGES='adobe-source-han-sans-jp-fonts ttf-hanazono adobe-source-han-sans-kr-fonts'
+readonly DEVELOPMENT_PACKAGES='base-devel git geany python3'
+readonly UTILITY_PACKAGES='htop powertop cpupower virtualbox vim'
+readonly EXTRA_PACKAGES='x11vnc openssh'
+
+readonly OPTIONAL_DEPENDENCIES='vte geany-plugins virtualbox-guest-iso'
+
+readonly SERVICES_TO_ENABLE='NetworkManager lightdm'
+
+function main() {
+    locale_generation
+    install_packages
+    enable_services
+    create_user
+}
+
+function die() {
+    echo "$1"
+    exit 1
+}
+
+function locale_generation() {
+    if ! locale-gen; then
+        die "Locale-gen failed."
+    fi
+}
+
+function install_packages() {
+    local packages_to_install="$GUI_PACKAGES $NETWORK_PACKAGES $SOUND_PACKAGES $INPUT_METHOD_PACKAGES $FONT_PACKAGES $DEVELOPMENT_PACKAGES $UTILITY_PACKAGES $EXTRA_PACKAGES"
+
+    if $USE_LINUX_LTS; then
+        if ! pacman -R --noconfirm linux; then
+            die "Removing linux package failed."
+        fi
+        packages_to_install="$packages_to_install $LINUX_LTS_PACKAGES nvidia-dkms"
+    else
+        packages_to_install="$packages_to_install nvidia"
+    fi
+
+    if $USE_BTRFS; then
+        packages_to_install="$packages_to_install $BTRFS_PACKAGES"
+    fi
+    
+    if $USE_NTFS; then
+        packages_to_install="$packages_to_install $NTFS_PACKAGES"
+    fi
+
+    if $USE_LINUX_LTS; then
+        packages_to_install="$packages_to_install nvidia-dkms"
+    else
+        packages_to_install="$packages_to_install nvidia"
+    fi
+    
+    if ! pacman -S --noconfirm $packages_to_install; then
+        die "Installing packages failed."
+    elif ! pacman -S --noconfirm --asdeps $OPTIONAL_DEPENDENCIES; then
+        die "Installing optional dependencies failed."
+    fi
+}
+
+function enable_services() {
+    if ! systemctl enable $SERVICES_TO_ENABLE; then
+        die "Enabling services failed."
+    fi
+}
+
+function create_user() {
+    if ! useradd -m -G wheel -s "$USER_SHELL" "$USER_NAME"; then
+        die "Creating new user failed."
+    fi
+}
+
+main
---
+++ install.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+# Arch Linux installation script.
+# Edit readonly variables below before run this script.
+# Should be run after mount root partition to $ROOT.
+
+# Directories and files
+readonly ROOT='/mnt'
+readonly BACKUP='./backup'
+readonly CHROOT_SCRIPT='chroot-install.sh'
+
+# Variables for new system
+readonly NEW_HOSTNAME=''
+readonly NEW_TIMEZONE='Asia/Seoul'
+readonly NEW_LANG='en_US.UTF-8'
+
+# Packages install flags
+readonly USE_LINUX_LTS='true'; export USE_LINUX_LTS # true to use linux-lts, false to use linux
+readonly USE_BTRFS='true'; export USE_BTRFS
+readonly USE_NTFS='true'; export USE_NTFS
+
+# Variables for user
+readonly USER_NAME=''; export USER_NAME
+readonly USER_SHELL='/usr/bin/fish'; export USER_SHELL
+
+function main() {
+    check_partitions_mounted
+    install_arch
+    system_configure
+    run_chroot_script
+    restore_user_home
+}
+
+function die() {
+    echo "$1"
+    exit 1
+}
+
+function check_partitions_mounted() {
+    if ! mountpoint -q "$ROOT"; then
+        die "Root partition is not mounted."
+    elif ! mountpoint -q "$ROOT/boot"; then
+        die "Boot partition is not mounted."
+    fi
+}
+
+function install_arch() {
+    if ! install -o root -g root -m 644 -T "$BACKUP/etc/pacman.d/mirrorlist" '/etc/pacman.d/mirrorlist'; then
+        die "Copying mirrorlist failed."
+    elif ! pacstrap "$ROOT" base; then
+        die "Arch linux install failed."
+    elif ! genfsatb -U "$ROOT" >> "$ROOT/etc/fstab"; then
+        die "Writing fstab failed."
+    fi
+}
+
+function system_configure() {
+    if ! ln -sf "/usr/share/zoneinfo/$NEW_TIMEZONE" "$ROOT/etc/localtime"; then
+        die "Timezone configure failed."
+    elif ! echo "$NEW_LANG" > "$ROOT/etc/locale.conf"; then
+        die "Locale configure failed."
+    elif ! echo "$NEW_HOSTNAME" > "$ROOT/etc/hostname"; then
+        die "Hostname setting failed."
+    fi
+
+    local new_filename
+
+    find "$BACKUP/etc" | while read file; do
+        new_filename="$ROOT/${file#$BACKUP/}"
+
+        if [ -f "$file" ]; then
+            if ! install -o root -g root -m 644 -T "$file" "$new_filename"; then
+                die "Failed to copy configure file $file"
+            fi
+        elif [ -d "$file" ]; then
+            if ! mkdir "$new_filename"; then
+                die "Making directory $new_filename failed."
+            fi
+        else
+            echo "File type error : $file"
+        fi
+    done
+}
+
+function run_chroot_script() {
+    arch-chroot "$ROOT" < "$CHROOT_SCRIPT"
+}
+
+function restore_user_home() {
+    if [ -d "$BACKUP/home/$USER_NAME" ]; then
+        if ! cp -r -p "$BACKUP/home/$USER_NAME" "$ROOT/home/$USER_NAME"; then
+            die "Copying user home directory failed."
+        elif ! chown -R 1000:1000 "$ROOT/home/$USER_NAME"; then
+            die "Change ownership of user home directory failed."
+        fi
+    fi
+}
+
+main
